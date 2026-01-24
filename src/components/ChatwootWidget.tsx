7f3124cbea354c838f32c92042a9ef242c2afde1
mhd-ajzal <ajzalmuhammed15@gmail.com>
2026-01-24 15:08:48 +0400

    Fix Chatwoot mixed content issue and configure Nixpacks

--- src/components/ChatwootWidget.tsx
+++ src/components/ChatwootWidget.tsx
@@ -6,14 +6,14 @@ export function ChatwootWidget() {
     return (
         <Script
             id="chatwoot-widget"
-            src="http://chatwoot-twcoc0go8gg8cgsg0kwcog4o.72.62.191.145.sslip.io/packs/js/sdk.js"
+            src="https://chatwoot-twcoc0go8gg8cgsg0kwcog4o.72.62.191.145.sslip.io/packs/js/sdk.js"
             strategy="lazyOnload"
             onLoad={() => {
                 // @ts-ignore
                 window.chatwootSDK.run({
                     websiteToken: "LFQQNMEq872v3i3jwoRmZPZ9",
                     baseUrl:
-                        "http://chatwoot-twcoc0go8gg8cgsg0kwcog4o.72.62.191.145.sslip.io",
+                        "https://chatwoot-twcoc0go8gg8cgsg0kwcog4o.72.62.191.145.sslip.io",
                 });
             }}
         />
