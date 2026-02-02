4fe2979facbfcf98a29ef7e72fccb974cd541d40
mhd-ajzal <ajzalmuhammed15@gmail.com>
2026-02-02 19:14:32 +0400

    Speed up partner image loop

--- src/components/PartnersSection.tsx
+++ src/components/PartnersSection.tsx
@@ -29,7 +29,7 @@ export function PartnersSection() {
                 <div className="flex gap-4 md:gap-24 animate-tech-scroll flex-nowrap min-w-full">
                     {/* Render 8 sets to ensure smooth loop on large screens */}
                     {[...PARTNERS, ...PARTNERS, ...PARTNERS, ...PARTNERS, ...PARTNERS, ...PARTNERS, ...PARTNERS, ...PARTNERS].map((partner, i) => (
-                        <div key={i} className="whitespace-nowrap flex items-center justify-center transition-all duration-500 cursor-default relative w-40 md:w-80 h-20 md:h-40 bg-white/5 rounded-lg border border-white/5 mx-2 md:mx-4 shrink-0">
+                        <div key={i} className="whitespace-nowrap flex items-center justify-center transition-all duration-100 cursor-default relative w-40 md:w-80 h-20 md:h-40 bg-white/5 rounded-lg border border-white/5 mx-2 md:mx-4 shrink-0">
                             <Image
                                 src={partner.logo}
                                 alt={partner.name}
